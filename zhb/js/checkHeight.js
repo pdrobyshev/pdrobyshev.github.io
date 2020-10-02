7e9fdaba5022e7cbff24cc7a9e46e9b6bbde85ab
pdrobyshev <pdrobyshev@wooppay.com>
2020-10-02 18:08:13 +0600

    asd

--- zhb/js/checkHeight.js
+++ zhb/js/checkHeight.js
@@ -1,15 +1,15 @@
-function checkHeight() {
-	if ($(window).height() < 780) {
-		$('.first-frame').addClass('first-frame--auto-height');
-	} else {
-		$('.first-frame').removeClass('first-frame--auto-height');
-	}
-}
-
-$(document).ready(function () {
-	checkHeight();
-});
-
-$(window).on('resize', function () {
-	checkHeight();
-});
+// function checkHeight() {
+// 	if ($(window).height() < 780) {
+// 		$('.first-frame').addClass('first-frame--auto-height');
+// 	} else {
+// 		$('.first-frame').removeClass('first-frame--auto-height');
+// 	}
+// }
+//
+// $(document).ready(function () {
+// 	checkHeight();
+// });
+//
+// $(window).on('resize', function () {
+// 	checkHeight();
+// });
--- zhb/js/scripts.js
+++ zhb/js/scripts.js
@@ -21,21 +21,21 @@ $('.call__modal-btn').on('click', function () {
 	$('body').addClass('body-overflow');
 });
 
-function checkHeight() {
-	if ($(window).height() < 780) {
-		$('.first-frame').addClass('first-frame--auto-height');
-	} else {
-		$('.first-frame').removeClass('first-frame--auto-height');
-	}
-}
-
-$(document).ready(function () {
-	checkHeight();
-});
-
-$(window).on('resize', function () {
-	checkHeight();
-});
+// function checkHeight() {
+// 	if ($(window).height() < 780) {
+// 		$('.first-frame').addClass('first-frame--auto-height');
+// 	} else {
+// 		$('.first-frame').removeClass('first-frame--auto-height');
+// 	}
+// }
+//
+// $(document).ready(function () {
+// 	checkHeight();
+// });
+//
+// $(window).on('resize', function () {
+// 	checkHeight();
+// });
 
 function checkSlider() {
 	if ($(window).outerWidth() >= 1360) {
