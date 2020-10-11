ded46c2b024bc22c64a4ce150a4977c7d702ecfa
pdrobyshev <pdrobyshev@wooppay.com>
2020-10-11 21:38:01 +0600

    asd

--- zhb/js/scripts.js
+++ zhb/js/scripts.js
@@ -186,28 +186,20 @@ $(document).on('keydown', function (e) {
 
 "use strict";
 
-$(document).ready(function () {
-	if ($(document).scrollTop() > 0) {
+function hideScrollButton() {
+	if ($(document).scrollTop() > 200) {
 		$('.call-buttons__scroll').hide();
 	} else {
 		$('.call-buttons__scroll').show();
 	}
+}
 
-	if ($(window).height() < 715) {
-		$('.call-buttons__scroll').css('bottom', '-21px');
-	}
+$(document).ready(function () {
+	hideScrollButton();
 
 	$(window).scroll(function () {
-		if ($(window).width() < 1160 && $(window).height() >= 715) {
-			if ($(document).scrollTop() > 0) {
-				$('.call-buttons__scroll').hide();
-			} else {
-				$('.call-buttons__scroll').show();
-			}
-		} else if ($(document).scrollTop() > 200) {
-			$('.call-buttons__scroll').hide();
-		} else {
-			$('.call-buttons__scroll').show();
+		if ($(window).width() >= 1160) {
+			hideScrollButton();
 		}
 	});
 
--- zhb/js/scroll-button.js
+++ zhb/js/scroll-button.js
@@ -1,27 +1,19 @@
 "use strict";
 
-$(document).ready(function () {
-	if ($(document).scrollTop() > 0) {
+function hideScrollButton() {
+	if ($(document).scrollTop() > 200) {
 		$('.call-buttons__scroll').hide();
 	} else {
 		$('.call-buttons__scroll').show();
 	}
+}
 
-	if ($(window).height() < 715) {
-		$('.call-buttons__scroll').css('bottom', '-21px');
-	}
+$(document).ready(function () {
+	hideScrollButton();
 
 	$(window).scroll(function () {
-		if ($(window).width() < 1160 && $(window).height() >= 715) {
-			if ($(document).scrollTop() > 0) {
-				$('.call-buttons__scroll').hide();
-			} else {
-				$('.call-buttons__scroll').show();
-			}
-		} else if ($(document).scrollTop() > 200) {
-			$('.call-buttons__scroll').hide();
-		} else {
-			$('.call-buttons__scroll').show();
+		if ($(window).width() >= 1160) {
+			hideScrollButton();
 		}
 	});
 
